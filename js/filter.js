ffb97442039a5cec73c0aaaa909e15fbe1b61cab
Pierre Jego <pierre.jego@jdev.fr>
2020-04-24 17:00:47 +0200

    Gestion des filtres cumulables

--- js/filter.js
+++ js/filter.js
@@ -1,17 +1,22 @@
 var filter = (function() {
 
   /**
-   * Property: _layerIdList
-   *  @type {}
+   * Property: _layersParams
+   *  @type {Map}
    */
-
   var _layersParams = new Map();
 
   /**
-   * Html template for filter panel
-   *
+   * Property: _currentFilters
+   *  @type {Map}
    */
-  var _filter_dialog = function(layerIdList, params) {
+  var _currentFilters = new Map();
+
+  /**
+   * Html template for filter panel, append to a global div
+   * @param {string} layerIdList if several layer create a different panel
+   */
+  var _filter_dialog = function(layerIdList) {
     var _dialog = [
       '<div id="advancedFilter" class="advancedFilter form-group">',
       '<div><h2>Filtres</h2></div>',
@@ -21,7 +26,8 @@ var filter = (function() {
   };
 
   /**
-   * Private Method: _configFilterableLayer expored as Public Methode configFilterableLayer
+   * Private Method: _configFilterableLayer
+   * expored as Public Methode configFilterableLayer
    *  add filter params for current layer
    *
    **/
@@ -154,30 +160,95 @@ var filter = (function() {
   };
 
   /**
-   * Private Method: _manageCheckbox
+   * Private Method: _addFilterElementToList
+   * @param {string} layerId The layer id to be filter
+   * @param {string} attribute The property key for filtering feature
+   * @param {object} value The value to filter can be String, Number, Boolean, Date,
+   * @param {String} type The value format to help filtering (text, date, boolean)
+   **/
+  var _addFilterElementToList = function(layerId, attribut, value, type) {
+
+    // Add filter only if there something to filter
+    if (layerId != null && attribut != null && value != null) {
+      var filtersByLayer = (_currentFilters.get(layerId) != null ? _currentFilters.get(layerId) : []);
+
+      var filter = {
+        layerId: layerId,
+        attribut: attribut,
+        value: value,
+        type: type
+      };
+
+      filtersByLayer.push(filter);
+      _currentFilters.set(layerId, filtersByLayer);
+    }
+  };
+
+  /**
+   * Private Method: _removeFilterElementFromList
+   * @param {string} layerId The layer id to be filter
+   * @param {string} attribute The property key for filtering feature
+   * @param {object} value The value to filter can be String, Number, Boolean, Date,
+   * @param {String} type The value format to help filtering (text, date, boolean)
+   **/
+  var _removeFilterElementFromList = function(layerId, attribut, value) {
+    var indexToRemove = -1;
+
+    var filtersByLayer = _currentFilters.get(layerId);
+    //search if value exist un currentFilters
+    if (filtersByLayer != undefined) {
+      filtersByLayer.forEach(function(filter, index, array) {
+        if (filter.attribut == attribut && (value == null || filter.value == value)) {
+          indexToRemove = index;
+        }
+      });
+
+      //only remove if exist
+      if (indexToRemove > -1) {
+        filtersByLayer.splice(indexToRemove, 1);
+        _currentFilters.set(layerId, filtersByLayer);
+      }
+    }
+
+  };
+
+  /**
+   * Private Method: _onValueChange
    *
-   * Action on checkbox
+   *  action when filter from filter panel change (checkbox, combobox, textarea or datapicker )
    **/
   var _onValueChange = function(element) {
-    var value = "";
-    var filtreInformation = element.id.split("-");
 
+    // get information for elment id ( type-layerid-attribut-index)
+    var filtreInformation = element.id.split("-");
     var type = filtreInformation[0];
     var layerId = filtreInformation[1];
     var attribut = filtreInformation[2];
+    var value = element.value;
+
+    // checkbox return index of value in _layersParams
     if (type == "filterCheck") {
       var indexValue = filtreInformation[3];
       value = _getValueFromInfo(layerId, attribut, indexValue);
+      // if check add filter, else remove filter
       if (element.checked == true) {
-        _filterFeatures(layerId, attribut, value);
+        _addFilterElementToList(layerId, attribut, value);
       } else {
-        _clearFilterFeatures(layerId, attribut, value);
+        _removeFilterElementFromList(layerId, attribut, value);
+      }
+    }
+    // combobox only one possible value so remove previous if exist
+    else if (type == "filterCombo") {
+      _removeFilterElementFromList(layerId, attribut, null);
+      // if value not the first text here ("Choississez...")
+      if(element.selectedIndex != 0){
+        _addFilterElementToList(layerId, attribut, value);
       }
     } else {
-      value = element.value;
-      _filterFeatures(layerId, attribut, value);
+      _addFilterElementToList(layerId, attribut, value);
     }
 
+    _filterFeatures(layerId);
   };
 
   /**
@@ -190,7 +261,7 @@ var filter = (function() {
   };
 
   /**
-   * Private Method: _getValueFromId
+   * Private Method: _getValueFromInfo
    *
    *
    **/
@@ -218,22 +289,38 @@ var filter = (function() {
    * Private Method: _filterFeature
    *
    **/
-  var _filterFeatures = function(layerId, key, value) {
+  var _filterFeatures = function(layerId) {
 
-    //TODO apply all feature at every filtering
-    if (value != null && value.length > 0) {
-      var featuresToBeFiltered = mviewer.getLayer(layerId).layer.getSource().getFeatures();
+    var filtersByLayer = _currentFilters.get(layerId);
+    var featuresToBeFiltered = mviewer.getLayer(layerId).layer.getSource().getFeatures();
 
-      featuresToBeFiltered.forEach(feature => {
-        // if feature map filter show it
-        if (feature.get(key) != null && feature.get(key).includes(value)) {
-          //feature.setStyle(null);
-        } else {
-          // hide
+    featuresToBeFiltered.forEach(feature => {
+
+      if (filtersByLayer.length > 0) {
+        var hideFeature = false;
+        //search if value exist un currentFilters
+        filtersByLayer.forEach(function(filter, index, array) {
+
+          // if feature map filter keep it
+          if (feature.get(filter.attribut) != null && feature.get(filter.attribut).includes(filter.value)) {
+            feature.setStyle(null);
+          } else {
+            // hide
+            // TODO save style for each feature
+            hideFeature = true;
+          }
+
+        });
+
+        if (hideFeature) {
           feature.setStyle(new ol.style.Style({}));
         }
-      });
-    }
+      }
+      // clear filter
+      else {
+        feature.setStyle(null);
+      }
+    });
   };
 
   /**
