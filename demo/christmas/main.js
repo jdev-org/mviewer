7fe7e636f6f74d2484f2cbef80d7cff6be3af6a6
gaetanbrl <gaetan.bruel@jdev.fr>
2022-12-21 17:59:15 +0100

    chrismas demo

--- demo/christmas/main.js
+++ demo/christmas/main.js
@@ -1,13 +1,11 @@
 var christmas = (function () {
     playSong = () => {
-        let audio = new Audio("/demo/christmas/Christmas_is_coming.mp3");
-        audio.play();
-        setInterval(() => playSong(), 50000)
+        let startPlayPromise = audioElem.play();
     }
     return {
         init: () => {
             playSong();
-        },
+        }
     };
   })();
   
