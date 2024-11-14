3939fe021cbecd9d76288c2ffbfab9a4de857840
raphael-dsc <raphael.desousacastro@gmail.com>
2024-11-14 12:33:21 +0100

    Add draw intercation

--- demo/addons/comments/js/comments.js
+++ demo/addons/comments/js/comments.js
@@ -18,24 +18,42 @@ var comments = (function () {
         // Get all configurations
         _config = mviewer.customComponents.comments.config.options.mviewer.comments;
         _map = mviewer.getMap();
+        _sourceDrawCmt = new ol.source.Vector();
 
         _commonDrawStyleCmt = _drawStyleBaseCmt(_config);
 
-        _vectorDrawCmt = new ol.layer.Vector ({
+        _vectorDrawCmt = new ol.layer.Vector({
             source: _sourceDrawCmt,
             style: function (feature) {
                 return _commonDrawStyleCmt[feature.getGeometry().getType()];
             },
         });
 
+        _vectorDrawCmt.set('mviewerid', 'testId');
+
+        mviewer.getMap().addLayer(_vectorDrawCmt);
+
         _initButtons();
+
+        document.addEventListener("keypress", function (event) {
+            if (event.key === "Escape" || event.key === "Esc") {
+                console.log("Esc");
+                console.log(event.key);
+                if (_drawIntCmt) {
+                    console.log("remove");
+                    mviewer.getMap().removeInteraction(_drawIntCmt);
+                }
+            }
+        });
     };
 
     var _initButtons = () => {
 
+        // Get the toolsbar and the new button to add
         let toolsBar = document.getElementById("toolstoolbar");
         let commentsBtnList = document.getElementById("commentsOptionsBtn");
 
+        // Add the button on the toolsbar
         toolsBar.appendChild(commentsBtnList);
 
         let availableTypesComments = ["Point", "LineString", "Polygon"];
@@ -66,7 +84,10 @@ var comments = (function () {
             divCommentsBtn.setAttribute("data-original-title", dataOriginalTitle);
 
             divCommentsBtn.addEventListener("click", function () {
-                _addDrawInteraction(type);
+                if (_drawIntCmt) {
+                    mviewer.getMap().removeInteraction(_drawIntCmt);
+                }
+                _addDrawInteractionCmt(type);
             });
 
             let divCommentBtnI = document.createElement("i");
@@ -80,8 +101,7 @@ var comments = (function () {
         let contentWrapper = document.getElementById("page-content-wrapper");
         contentWrapper.appendChild(commentsToolsOptions);
 
-        let commentsOptionBtn = document.getElementById("commentsOptionsBtn");
-        commentsOptionBtn.addEventListener("click", function () {
+        commentsBtnList.addEventListener("click", function () {
             _toggle();
         });
     };
@@ -103,71 +123,67 @@ var comments = (function () {
         });
         
         return {
-            Polygon: [
+            "Polygon": [
                 new ol.style.Style({
                     stroke: commonLineStrokeCmt,
                     fill: commonFillCmt,
                 }),
-                new ol.style.Style({
-                    image: new ol.style.Circle({
-                        radius: 5,
-                        fill: pointFillCmt,
-                        stroke: commonPointStrokeCmt,
-                    }),
-                    geometry: function (feature) {
-                        const coordinates = feature.getGeometry().getCoordinates()[0];
-                        if (coordinates.length > 2) {
-                            return new ol.geom.MultiPoint(coordinates);
-                        }
-                    },
-                }),
+                // new ol.style.Style({
+                //     image: new ol.style.Circle({
+                //         radius: 5,
+                //         fill: pointFillCmt,
+                //         stroke: commonPointStrokeCmt,
+                //     }),
+                //     geometry: function (feature) {
+                //         const coordinates = feature.getGeometry().getCoordinates()[0];
+                //         if (coordinates.length > 2) {
+                //             return new ol.geom.MultiPoint(coordinates);
+                //         }
+                //     },
+                // }),
             ],
-            LineString: [
+            "LineString": [
                 new ol.style.Style({
                     stroke: commonLineStrokeCmt,
-                    fill: commonFillCmt,
+                    // fill: commonFillCmt,
                 }),
-                new ol.style.Style({
-                    image: new ol.style.Circle({
-                        radius: 5,
-                        fill: pointFillCmt,
-                        stroke: commonPointStrokeCmt,
-                    }),
-                }),
+                // new ol.style.Style({
+                //     image: new ol.style.Circle({
+                //         radius: 5,
+                //         fill: pointFillCmt,
+                //         stroke: commonPointStrokeCmt,
+                //     }),
+                // }),
             ],
-            Point: new ol.style.Style({
+            "Point": new ol.style.Style({
                 image: new ol.style.Circle({
                     radius: 6,
-                        fill: pointFillCmt,
-                        stroke: commonPointStrokeCmt,
-                 }),
+                    fill: pointFillCmt,
+                    stroke: commonPointStrokeCmt,
+                }),
             }),
         };
     }
 
     var _addDrawInteractionCmt = (type) => {
-        console.log("addDrawInteraction");
-        console.log(type);
-
-        _drawIntCmt = new DeleteOnRightClick({
+           
+        _drawIntCmt = new ol.interaction.Draw({
             source: _sourceDrawCmt,
             type: type,
             style: _commonDrawStyleCmt[type],
         });
 
-        _map.addInteraction(_drawIntCmt);
+        mviewer.getMap().addInteraction(_drawIntCmt);
     };
 
     var _toggle = function () {
         if (_modCommentsEnabled) {
           // If the module comments is active
           // Launch comments.disable
-          //mviewer.unsetTool("comments");
           _disableDrawToolCmt();
         } else {
           // If the module comments is not active
           // Launch comments.enable
-          //mviewer.setTool("comments");
           _enableDrawToolCmt();
         }
     };
@@ -185,6 +201,10 @@ var comments = (function () {
 
     var _disableDrawToolCmt = () => {
         _modCommentsEnabled = false;
+
+        mviewer.getMap().removeInteraction(_drawIntCmt);
+
+        _sourceDrawCmt.clear();
 
         _clearDrawToolCmt();
     };
@@ -208,7 +228,7 @@ var comments = (function () {
 
     return {
         init: _initCommentsTool,
-        toggle: _initButtons,
+        // toggle: _initButtons,
         // enable: _enableCommentsTool,
         // disable: _disableCommentsTool,
     };
